fac324cf87ddf6794591ef6dcbfb267e3bed8b41
kangsan <devstar1223@gmail.com>
2026-09-13 16:15:56 +0900

    📦 build(server): Spring Boot 서버 초기화

---
+++ server/src/test/java/com/devstar1223/projectaka/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devstar1223.projectaka.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
